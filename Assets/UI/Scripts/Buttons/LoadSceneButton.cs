b678898f9635dc474de8507991a2745804c02eb9
Erika <2530460+xne@users.noreply.github.com>
2025-09-29 18:25:44 -0400

    Update LoadSceneButton.cs

--- Assets/UI/Scripts/Buttons/LoadSceneButton.cs
+++ Assets/UI/Scripts/Buttons/LoadSceneButton.cs
@@ -1,11 +1,20 @@
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadSceneButton : Button
 {
     public string scene;
 
-    protected override void OnClick()
+    protected override void Start()
     {
-        SceneManager.LoadScene(scene);
+        if (string.IsNullOrEmpty(scene))
+        {
+            Debug.LogError($"{name} does not have a scene reference");
+            return;
+        }
+
+        base.Start();
     }
+
+    protected override void OnClick() => SceneManager.LoadScene(scene);
 }

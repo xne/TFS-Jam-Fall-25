be2f3bd3a30682f232b6e9c0911d2df214fcaaab
Erika <2530460+xne@users.noreply.github.com>
2025-09-29 20:49:23 -0400

    Add level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private string level = "Scenes/Levels/Level1";
+    public string Level => level;
+
+    private bool isLoading = false;
+
+    private void Start()
+    {
+        LoadLevel(level);
+    }
+
+    public void LoadLevel(string nextLevel)
+    {
+        if (isLoading)
+        {
+            Debug.LogWarning("Already loading a level");
+            return;
+        }
+
+        if (string.IsNullOrEmpty(nextLevel))
+        {
+            Debug.LogWarning("Level is null");
+            return;
+        }
+
+        if (!Application.CanStreamedLevelBeLoaded(nextLevel))
+        {
+            Debug.LogWarning($"Level cannot be loaded");
+            return;
+        }
+
+        StartCoroutine(LoadLevelCoroutine(nextLevel));
+    }
+
+    private IEnumerator LoadLevelCoroutine(string nextLevel)
+    {
+        isLoading = true;
+
+        if (SceneManager.GetSceneByPath(level).isLoaded)
+        {
+            var unloadOp = SceneManager.UnloadSceneAsync(level);
+            while (!unloadOp.isDone)
+                yield return null;
+        }
+
+        var loadOp = SceneManager.LoadSceneAsync(nextLevel, LoadSceneMode.Additive);
+        while (!loadOp.isDone)
+            yield return null;
+
+        level = nextLevel;
+
+        isLoading = false;
+    }
+}

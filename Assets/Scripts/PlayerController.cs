5e5e8768e6d2b94690b6f8ff8e5bd39ad9a677aa
Erika <2530460+xne@users.noreply.github.com>
2025-09-30 18:12:44 -0400

    Add attacking

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,46 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 2f;
+    public float attackTime = 1f;
 
     private Animator animator;
 
     private InputAction moveAction;
+
+    private bool isAttacking = false;
+    private float attackTimer = 0f;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
 
         moveAction = InputSystem.actions.FindAction("Move");
+        InputSystem.actions.FindAction("Attack").performed += AttackAction_Performed;
     }
 
     private void Update()
     {
-        var input = moveAction.ReadValue<Vector2>();
+        if (isAttacking)
+        {
+            if (attackTimer < attackTime)
+            {
+                attackTimer += Time.deltaTime;
+                return;
+            }
+            else
+            {
+                isAttacking = false;
+                attackTimer = 0f;
+            }
+        }
+
+        var moveInput = moveAction.ReadValue<Vector2>();
+
+        Move(moveInput);
+    }
+
+    private void Move(Vector2 input)
+    {
         transform.position += moveSpeed * Time.deltaTime * (Vector3)input;
 
         if (Mathf.Approximately(input.x, 0f) && Mathf.Approximately(input.y, 0f))
@@ -32,5 +57,12 @@ public class PlayerController : MonoBehaviour
 
             animator.Play("Move");
         }
+    }
+
+    private void AttackAction_Performed(InputAction.CallbackContext obj)
+    {
+        isAttacking = true;
+
+        animator.Play("Attack");
     }
 }
